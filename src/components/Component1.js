f3e54551b69ea5b7724689edb38fc8493a097445
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-17 17:55:19 +0800

    added new changes

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"; 
-import { json } from "react-router-dom";
 
 
 
 export const Component1 = () => {
     const [datas, setDatas] = useState([])
-    const postDatas = () => {
-     fetch(`https://pokeapi.co/api/v2/pokemon/`).then(response => response.json()).then((json) => setDatas(json.results));
+    const postDatas = async() => {
+     const axiosResponse = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        setDatas(axiosResponse.data.results)
     }
 
     useEffect(() => {
