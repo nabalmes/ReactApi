bdb2c66d958cd6f38e08b36f27db04de9d70d5ce
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-11 14:21:55 +0800

    used axios

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -7,9 +7,9 @@ export const Component2 = () => {
     const  [search, setSearch] = useState('')
 
     const [axiosData, setAxiosData] = useState([])
-    const postAxiosData = () => {
-        
-        fetch(`https://pokeapi.co/api/v2/pokemon/`).then(response => response.json()).then((json) => setAxiosData(json.results))
+    const postAxiosData = async () => {
+        const axiosResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+        setAxiosData(postAxiosData.data.results)
     }
 
     useEffect(() => {
