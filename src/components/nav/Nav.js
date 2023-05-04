efb216d93a211a6f0e2057e1f8eaea60b15291af
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-05-04 18:41:49 +0800

    fix bugs

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,9 +3,9 @@ import './Nav.css'
 import { NavLink } from "react-router-dom";
 
 function navBar() {
-    return(
+    return (
         <div className="navBar-container">
-            <div className="navBar">   
+            <div className="navBar">
                 <NavLink className="nav-link" to="/">Component1</NavLink>
                 <NavLink className="nav-link" to="/component1">Component2</NavLink>
                 <NavLink className="nav-link" to="/component2">Component3</NavLink>
