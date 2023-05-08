8f8a8cfc86bcefae2e004e8d45187dd22d2bbf80
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-05-08 18:38:14 +0800

    fixed structure

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,22 +4,22 @@ import axios from "axios";
 export const Component = () => {
     const [data1, setData1] = useState([])
     const postData1 = async () => {
-      const axiosData = axios.get(`https://pokeapi.co/api/v2/pokemon/`)
-      setData1(axiosData.data.results)
+        const axiosData = axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+        setData1(axiosData.data.results)
 
-    useEffect(() => {
-        postData1()
-    }, [])
+        useEffect(() => {
+            postData1()
+        }, [])
 
 
-    return (
-        <section>
-            <h1>Component</h1>
-            {
-                data1.map((data, index) =>
-                    <div key={index}>{data.url}</div>
-                )
-            }
-        </section>
-    )
-}
+        return (
+            <section>
+                <h1>Component</h1>
+                {
+                    data1.map((data, index) =>
+                        <div key={index}>{data.url}</div>
+                    )
+                }
+            </section>
+        )
+    }
