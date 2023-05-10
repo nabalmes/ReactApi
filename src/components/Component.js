6dc8770c1aae2e6e4d4fde00f41674cd30d21f50
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-05-10 18:25:37 +0800

    fixed bugs

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,7 +4,8 @@ import axios from "axios";
 export const Component = () => {
     const [data1, setData1] = useState([])
     const postData1 = async () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/`).then(response => response.json()).then((json) => setData1(json.results))
+       const axiosData = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+       setData1(axiosData.data.results)
 
         useEffect(() => {
             postData1()
