692915a1c7b48e838eb2d4dfc62c2af22d57106e
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-05-12 19:59:54 +0800

    used fetch

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,21 +4,22 @@ import axios from "axios";
 export const Component = () => {
     const [data1, setData1] = useState([])
     const postData1 = async () => {
-       fetch(`https://pokeapi.co/api/v2/pokemon/`).then(response => response.json()).then((json) => setData1(json.results))
-
-        useEffect(() => {
-            postData1()
-        }, [])
-
-
-        return (
-            <section>
-                <h1>Component</h1>
-                {
-                    data1.map((data, index) =>
-                        <div key={index}>{data.url}</div>
-                    )
-                }
-            </section>
-        )
+        const axiosData = axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+        setData1(axiosData.data.results)
     }
+    useEffect(() => {
+        postData1()
+    }, [])
+
+
+    return (
+        <section>
+            <h1>Component</h1>
+            {
+                data1.map((data, index) =>
+                    <div key={index}>{data.url}</div>
+                )
+            }
+        </section>
+    )
+}
