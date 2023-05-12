fe30599d5b85487a9f8e54055a773cb6028f6eef
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-05-12 20:00:12 +0800

    used axios

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"; 
+import axios from "axios";
 import { json } from "react-router-dom";
 
 
 
 export const Component1 = () => {
     const [datas, setDatas] = useState([])
-    const postDatas = async() => {
-    const axiosData1 = axios.get(`https://pokeapi.co/api/v2/pokemon/`)
-    setDatas(axiosData1.data.results)
+    const postDatas = async () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/`).then(response => response.json()).then((json) => setDatas(json.results))
 
-    useEffect(() => {
-        postDatas()
-    }, [])
-    return(
-        <section>
-            <h1>Component1</h1>
-            {
-                datas.map((data, index) =>
-                <h1 key={index}>{data.name}</h1>
-                )
-            }         
-        </section>
-    )
-}
+        useEffect(() => {
+            postDatas()
+        }, [])
+        return (
+            <section>
+                <h1>Component1</h1>
+                {
+                    datas.map((data, index) =>
+                        <h1 key={index}>{data.name}</h1>
+                    )
+                }
+            </section>
+        )
+    }
